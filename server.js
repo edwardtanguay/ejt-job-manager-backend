a35b9b58169bdf443a43663c2ea9e4bebeff192c
Edward Tanguay (windows) <edward@tanguay.info>
2022-07-12 07:26:37 +0200

    connection to online database

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import { JobSource } from './models/JobSource.js';
+
+dotenv.config();
+
+const MONGODB_URI =
+	process.env.MONGODB_URI || 'mongodb://localhost/ejt-job-manager';
+
+mongoose.connect(MONGODB_URI, (err) => {
+	if (err) {
+		console.log({
+			error: "Cannot connect to MongoDB database.",
+			err: `"${err}"`
+		});
+	} 
+});
 
 const app = express();
 const port = 3044;
 
 app.get('/', (req, res) => {
     res.send('<h1>Job-Manager API</h1>');
+});
+
+app.get('/job-sources', async (req, res) => {
+	const jobSources = await JobSource.find();
+	res.status(200).json(jobSources);
 });
 
 app.listen(port, () => {
